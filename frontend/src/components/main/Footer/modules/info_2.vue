f81963538fa0b0c1902895214d93394674bc05b8
KIM Shingu <creation07@naver.com>
2018-12-05 11:58:10 +0900

    18-12-05

--- frontend/src/components/main/Footer/modules/info_2.vue
+++ frontend/src/components/main/Footer/modules/info_2.vue
@@ -1,13 +1,16 @@
 <template>
-  <span>본사주소
-    <br>주소
-    <br>전화번호
+  <span>
+
+    <b>365고객센터</b> | 전자금융거래분쟁처리담당
+    <br> <b>02-851-4790</b>
+    <br> 686-53 프레콘빌딩 501호, 동작구 서울특별시
+    <br> Fax : 02-851-4790| email : help@soojin.com
   </span>
 </template>
-
 <script>
 export default {};
 </script>
 
 <style>
+
 </style>
