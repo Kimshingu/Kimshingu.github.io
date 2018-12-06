215cf8c06ac232838ff2c0323ed2e8809a1e0503
KIM Shingu <creation07@naver.com>
2018-12-06 16:44:00 +0900

    Sean 12-06 #2

--- frontend/src/components/search/modules/Block.vue
+++ frontend/src/components/search/modules/Block.vue
@@ -1,6 +1,6 @@
 <template lang="html">
 <div>
-  <img @click="goDetail" src="https://via.placeholder.com/180x200" alt="">
+  <img @click="goDetail" :src="getImgUrl(data.id)" alt="" width="200" height="200">
       <li>{{localeCharge!='0'?localeCharge+' 원':'무료배송'}}</li>
       <li>{{data.name}}</li>
       <li>{{localePrice}} 원</li>
@@ -21,7 +21,11 @@ export default {
     goDetail: function() {
       sessionStorage.setItem("p_id", this.data.id);
       this.$router.push("/product");
-    }
+    },
+    getImgUrl: function(id) {
+    var images = require.context('../../productimg/', false, /\.jpg$/)
+    return images('./' + id + ".jpg")
+  }
   },
   props: ["data"]
 };
