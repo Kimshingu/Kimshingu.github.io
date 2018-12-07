ffad10fcf75ab9e1b23e8aef9a180378d4a4aa94
Gonhong Cheon <take91@naver.com>
2018-12-07 17:29:22 +0900

    181207 update by gonhong #4

--- frontend/src/components/order/Footer/Footer.vue
+++ frontend/src/components/order/Footer/Footer.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="orderFooter" class="text-center">
-    <Button id="buyButton" type="button" class="btn btn-color1" v-b-modal.modal1>결제하기</Button>
-    <div v-if="isSelected">
-      <Modal :prop="prop"></Modal>
-    </div>
+    <Button
+      v-if="isSelected"
+      id="buyButton"
+      type="button"
+      class="btn btn-color1"
+      v-b-modal.modal1
+    >결제하기</Button>
+    <Button id="buyButton_fake" v-else type="button" class="btn btn-color1">결제하기</Button>
+    <Modal :prop="prop"></Modal>
   </div>
 </template>
 
@@ -19,31 +24,33 @@ export default {
   components: {
     Modal: Modal
   },
-  methods: {
+  computed: {
     isSelected() {
-      if (this.Selected.radioSelected == "card") {
-        if (this.Selected.cardSelected) {
-          return true;
-        } else {
-          return false;
-        }
-      } else if (this.Selected.radioSelected == "mutong") {
-        if (this.Selected.bankSelected) {
-          return true;
-        } else {
-          return false;
-        }
-      } else if (this.Selected.radioSelected == "account") {
-        if (this.Selected.bankSelected) {
-          return true;
-        } else {
-          return false;
-        }
-      } else if (this.Selected.radioSelected == "phone") {
-        if (this.Selected.phoneSelected) {
-          return true;
-        } else {
-          return false;
+      if (this.Selected) {
+        if (this.Selected.radioSelected == "card") {
+          if (this.Selected.cardSelected) {
+            return true;
+          } else {
+            return false;
+          }
+        } else if (this.Selected.radioSelected == "mutong") {
+          if (this.Selected.bankSelected) {
+            return true;
+          } else {
+            return false;
+          }
+        } else if (this.Selected.radioSelected == "account") {
+          if (this.Selected.bankSelected) {
+            return true;
+          } else {
+            return false;
+          }
+        } else if (this.Selected.radioSelected == "phone") {
+          if (this.Selected.phoneSelected) {
+            return true;
+          } else {
+            return false;
+          }
         }
       }
     }
@@ -69,6 +76,9 @@ export default {
   margin-bottom: 50px;
 }
 #buyButton {
+  width: 500px;
+}
+#buyButton_fake {
   width: 500px;
 }
 </style>
