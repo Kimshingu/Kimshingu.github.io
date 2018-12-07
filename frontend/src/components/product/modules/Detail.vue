dc8c683f0e08b98d24a62ad423445de28a171f8c
Gonhong Cheon <take91@naver.com>
2018-12-07 16:42:48 +0900

    181207 update by gonhong #3

--- frontend/src/components/product/modules/Detail.vue
+++ frontend/src/components/product/modules/Detail.vue
@@ -3,7 +3,8 @@
     <h4>{{prop.name}}</h4>
     <hr>
     <li class="productPrice">{{localePrice}} 원</li>
-    <li class="deliveryCharge" >{{localeCharge!='0'?localeCharge+' 원':'무료배송'}}</li>
+    <li class="deliveryCharge">{{localeCharge!='0'?'배송비 : '+localeCharge+' 원':'무료배송'}}</li>
+    <br>
     <!-- <li>{{prop.seller}}</li> -->
     <b-form-select v-model="selectedSize" value="null" id="size" :options="sizeOp" class="mb-3"/>
     <b-form-select v-model="selectedColor" value="null" id="color" :options="colorOp" class="mb-3"/>
@@ -49,11 +50,10 @@ export default {
       var self = this;
 
       if (sessionStorage.getItem("user") === null) {
-        alert('회원만 구매가능한 상품입니다. 로그인 화면으로 이동합니다.')
+        alert("회원만 구매가능한 상품입니다. 로그인 화면으로 이동합니다.");
         this.$router.push("/login");
         return;
-      }
-      else if (
+      } else if (
         this.selectedCount !== null &&
         (this.selectedSize !== null && this.selectedColor !== null)
       ) {
@@ -74,16 +74,14 @@ export default {
       } else {
         alert("옵션을 모두 선택해주세요.");
       }
-
     },
     goOrder() {
-
       this.prop.color = this.selectedColor;
       this.prop.size = this.selectedSize;
       this.prop.hitcount = this.selectedCount;
 
       if (sessionStorage.getItem("user") === null) {
-        alert('회원만 구매가능한 상품입니다. 로그인 화면으로 이동합니다.')
+        alert("회원만 구매가능한 상품입니다. 로그인 화면으로 이동합니다.");
         this.$router.push("/login");
       } else if (
         this.selectedCount !== null &&
@@ -103,5 +101,13 @@ export default {
 <style>
 li {
   list-style: none;
+}
+
+.productPrice {
+  font-size: 1.2em;
+}
+
+.deliveryCharge {
+  font-size: 0.8em;
 }
 </style>
